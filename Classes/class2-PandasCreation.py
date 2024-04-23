a2f1ac66b25ef5c33fa78e15bc2ba7e16fd462a6
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2024-04-23 17:46:59 +0530

    Update class2-PandasCreation.py

--- Classes/class2-PandasCreation.py
+++ Classes/class2-PandasCreation.py
@@ -1,1 +1,7 @@
-
+intro = """CREATION OF DATAFRAME 
+            Import pandas
+            1.Dataframe Can be Created Empty.
+            2.Created From List.
+            3.Created from Tuples
+            4.Created From Pandas Series
+"""
